7440f3fad283738cd821ec156e735ebe5ce07d37
two-woods <1083965690@qq.com>
2020-08-27 17:59:11 +0800

    double  Link  List version one *createDoubleList *treavlList *insertByData

--- data_structure/Linear_list/double_linked_list.go
+++ data_structure/Linear_list/double_linked_list.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type doubleElemType int
 type doubleLinkList struct {
@@ -14,7 +16,20 @@ func createDoubleHead() (head doubleLinkList) {
 	return
 }
 
-func insert() {
+func insertByData(head *doubleLinkList, x doubleElemType, item doubleElemType) (s string) {
+	s = "insert success"
+	p := head.rlink
+	for p != nil && p.data != x {
+		p = p.rlink
+	}
+	if p == nil {
+		s = fmt.Sprintf("no have node which data is %d", x)
+		return
+	}
+	q := &doubleLinkList{0, p, p.rlink}
+	p.rlink.llink = q
+	p.rlink = q
+	return s
 }
 
 func delete() {
@@ -23,18 +38,51 @@ func delete() {
 func createDoubleList(head *doubleLinkList, initList []int) {
 	p := head
 	for _, v := range initList {
-		q := &doubleLinkList{doubleElemType(v), nil, nil}
-		p.rlink = q
+		//q := &doubleLinkList{doubleElemType(v), nil, nil}
+		//p.rlink = q
+		//q.llink = p
+		//p = q
+		q := doubleLinkList{doubleElemType(v), nil, nil}
+		p.rlink = &q
 		q.llink = p
-		p = q
+		p = &q
 	}
-	fmt.Println("2312312", head.rlink.data)
+
 }
 
+func treavlList(head doubleLinkList) {
+
+	//if head.rlink == &head{                    //此处要用main.doubleLinkList值类型来比较，而非指针类型
+	if *head.rlink == head {
+		fmt.Println("no node")
+		return
+	}
+	fmt.Print("treavl : ")
+	r := &head
+	p := head.rlink
+	for p != nil {
+		fmt.Print(p.data, ",")
+		r = p
+		p = p.rlink
+	}
+	fmt.Print("reverse traveral:")
+	//fmt.Println("0-0-0-0--0-0-",r.llink)
+	for *r != head {
+		fmt.Print(r.data, ",")
+		r = r.llink
+	}
+
+}
 func main() {
 	head := createDoubleHead()
 	initList := []int{1, 2, 3, 4}
+	//initList := []int{}
 	createDoubleList(&head, initList)
+	treavlList(head)
+
+	fmt.Print("\nNow ,we insert data after 2,")
+	insertByData(&head, 2, 0)
+	treavlList(head)
 
 	//x =
 	//insert(head,x,item)
